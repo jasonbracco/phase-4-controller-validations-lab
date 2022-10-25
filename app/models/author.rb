84c167c2692b1a72ca3b00b701f58f01c0b12a58
Jason Bracco <bracco.jason@gmail.com>
2022-10-24 21:59:10 -0400

    commit tree 4b3894da006bffcd3809ee0a1097a1076210e2b1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :email, uniqueness: true
+
+    def create
+        author = Author.create!(author_params)
+        render json: author, status: :created
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    def update
+        post = Post.find(params[:id])
+        post.update!(post_params)
+        render json: post
+    end
+
+
 end
